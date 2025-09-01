7a37e05366ef73a1d2cc8c4ec262b01960727430
Stathis Gennatas <gennatas@gmail.com>
2025-09-01 13:24:30 -0700

    init imports

---
+++ R/kaimana-package.R
@@ -0,0 +1,19 @@
+# kaimana-package.R
+# ::kaimana::
+# 2025 EDG rtemis.org
+
+#' \pkg{kaimana}: Agentic AI for the rtemis ecosystem
+#'
+#' @description
+#' The kaimana R package provides functionality for using LLMs and Agentic AI within the rtemis
+#' ecosystem.
+#'
+#' @section Online Documentation and Vignettes:
+#' <https://kaimana.rtemis.org>
+#'
+#' @name kaimana-package
+#' @import rtemis data.table S7
+#' @importFrom utils sessionInfo
+#' @importFrom rtemis repr show_ls get_output_type check_inherits
+"_PACKAGE"
+NULL
