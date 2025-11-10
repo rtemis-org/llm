9b0b31db88dabb45b968553ef5f490e731d0fc89
Stathis Gennatas <gennatas@gmail.com>
2025-11-09 18:54:49 -0800

    update utils_ollama

--- R/utils_ollama.R
+++ R/utils_ollama.R
@@ -9,61 +9,88 @@
 # References
 # https://hauselin.github.io/ollama-r/
 
+# %% .ollama_api_tags() ----
+.ollama_api_tags <- function(
+  base_url = OLLAMA_URL_DEFAULT,
+  output = c("list", "json")
+) {
+  output <- match.arg(output)
+  req <- httr2::request(paste0(base_url, "/api/tags")) |>
+    httr2::req_method("GET")
+  res <- httr2::req_perform(req)
+  # Check response status
+  httr2::resp_check_status(res)
+  if (output == "list") {
+    httr2::resp_body_json(res)
+  } else {
+    httr2::resp_body_string(res)
+  }
+} # /kaimana::.ollama_api_tags
+
+# %% ollama_list_models() ----
 #' List Ollama Models
 #'
 #' @return Character vector: Model names.
 #'
 #' @author EDG
 #' @export
-ollama_list_models <- function() {
-  ollamar::list_models(output = "text")
-} # /ollama_list_models
+ollama_list_models <- function(
+  base_url = OLLAMA_URL_DEFAULT
+) {
+  res <- .ollama_api_tags(base_url = base_url, output = "list")
+  # Format response using httr2
+  sapply(res[["models"]], function(x) x[["model"]])
+} # /kaimana::ollama_list_models
 
 
+# %% ollama_get_model_info() ----
 #' Get Ollama Model Info
 #'
 #' @param model Character: Name of model. If NULL, all models are returned.
-#' @param output Character: Type of output to return: "df", "resp", "jsonlist", "raw", "text"
+#' @param base_url Character: Base URL of Ollama server.
 #'
-#' @return Depending on `output`.
+#' @return List with model info if `x` is defined, data.table with all models' info otherwise.
 #'
 #' @author EDG
 #' @export
-ollama_get_model_info <- function(model = NULL, output = "df") {
-  models <- ollamar::list_models(output = output)
-  if (is.null(model)) {
-    return(models)
+ollama_get_model_info <- function(x = NULL, base_url = OLLAMA_URL_DEFAULT) {
+  models <- .ollama_api_tags(base_url = base_url, output = "list")[["models"]]
+  models <- data.table(do.call(rbind, lapply(models, unlist)))
+  if (is.null(x)) {
+    out <- models
   } else {
-    model_info <- models[models$name == model, , drop = FALSE]
-    if (nrow(model_info) == 0) {
-      stop("Model ", model, " not found.")
+    out <- models[name == x]
+    if (nrow(out) == 0) {
+      cli::cli_abort("Model ", x, " not found.")
     }
-    if (nrow(model_info) == 0) {
-      stop("Model ", model, " not found.")
-    }
-    return(model_info)
+    out <- as.list(out)
   }
+  out
 } # /ollama_get_model_info
 
+
+# %% ollama_check_model() ----
 #' Check Ollama Model is Available
 #'
-#' @param model Character: Name of model.
+#' @param x Character: Name of model.
 #'
-#' @return Logical: TRUE if model is available, FALSE otherwise.
+#' @return NULL, invisibly if model is available; otherwise throws an error.
 #'
 #' @author EDG
 #' @export
-ollama_check_model <- function(model) {
-  if (model %in% ollama_list_models()) {
+ollama_check_model <- function(x) {
+  if (x %in% ollama_list_models()) {
     invisible(NULL)
   } else {
-    stop(
+    cli::cli_abort(
       "Model ",
-      model,
-      " is not available. Please check the model name and install if necessary."
+      x,
+      " is not available. Please check the model name and pull it if necessary.\n",
+      "List available models with `ollama_list_models()`."
     )
   }
-} # /ollama_check_model
+} # /kaimana::ollama_check_model
+
 
 #' Message Ollama
 #'
@@ -109,7 +136,6 @@ ollama_chat <- function(
 #' @return Character: Model response depending on `output_type`.
 #'
 #' @author EDG
-#' @export
 #'
 #' @details
 #' The `output_schema` argument is a list that defines the expected format of the response.
