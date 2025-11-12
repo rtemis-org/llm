dd79f00921f3e31f8ffb38307d81633315107659
Stathis Gennatas <gennatas@gmail.com>
2025-11-12 14:25:40 -0800

    up test

--- tests/testthat/test_AgentState.R
+++ tests/testthat/test_AgentState.R
@@ -14,51 +14,33 @@ test_that("InMemoryAgentState class works", {
   testthat::expect_true(S7_inherits(state, InMemoryAgentState))
 }) # /InMemoryAgentState
 
-# %% append_message.AgentState ----
-imas <- InMemoryAgentState()
-imas
-system_prompt <- "You are a meticulous research assistant named Kaimana."
-sm <- SystemMessage(
-  name = "KMN",
-  content = system_prompt
-)
-sm
-append_message(imas, sm)
-imas
-im <- InputMessage(
-  name = "User1",
-  content = "Hello, agent!"
-)
-im
-append_message(imas, im)
-imas
-tool_response <- ToolMessage(
-  name = "addition",
-  content = "The result of adding 2 and 3 is 5."
-)
-append_message(imas, tool_response)
-im2 <- InputMessage(
-  name = "User1",
-  content = "The tools returned: The result of adding 2 and 3 is 5. Use the tool response to answer the original question."
-)
-append_message(imas, im2)
+# %% append_message.AgentState + get_messages.AgentState ----
 
-# %% get_messages.AgentState ----
-msgs <- get_messages(imas)
-msgs
-length(msgs)
-
-# %%
-z <- new.env()
-z
-
-sm
-
-# Append sm to z
-z[[1]] <- sm
-
-
-im <- InputMessage(
-  name = "User1",
-  content = "Hello, world!"
-)
+test_that("append_message and get_messages for AgentState work", {
+  imas <- InMemoryAgentState()
+  sm <- SystemMessage(
+    name = "KMN",
+    content = "You are a meticulous research assistant."
+  )
+  im <- InputMessage(
+    name = "User1",
+    content = "Hello, agent!"
+  )
+  tool_response <- ToolMessage(
+    name = "addition",
+    content = "The result of adding 2 and 3 is 5."
+  )
+  im2 <- InputMessage(
+    name = "User1",
+    content = "The tools returned: The result of adding 2 and 3 is 5. Use the tool response to answer the original question."
+  )
+  append_message(imas, sm)
+  testthat::expect_equal(length(get_messages(imas)), 1)
+  append_message(imas, im)
+  testthat::expect_equal(length(get_messages(imas)), 2)
+  append_message(imas, tool_response)
+  testthat::expect_equal(length(get_messages(imas)), 3)
+  append_message(imas, im2)
+  msgs <- get_messages(imas)
+  testthat::expect_equal(length(msgs), 4)
+}) # /append_message + get_messages
