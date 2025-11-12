bfdeb39e1d88ee7ef32658c0f5342c53927f2b62
Stathis Gennatas <gennatas@gmail.com>
2025-11-12 13:11:01 -0800

    cleanup

--- R/tool_duckduckgo_ia.R
+++ R/tool_duckduckgo_ia.R
@@ -80,72 +80,7 @@ query_duckduckgo_ia <- function(
     return(jsonlite::toJSON(dat, pretty = FALSE, auto_unbox = TRUE))
   }
   dat
-} # /query_duckduckgo_ia
-
-
-#' Scrape DuckDuckGo Search Results
-#'
-#' @param query Character: Search query.
-#'
-#' @return JSON or data.table with response
-#'
-#' @author EDG
-#'
-#' @export
-query_duckduckgo <- function(
-  query,
-  ia = NULL,
-  return_all = FALSE,
-  output = c("json", "data.table")
-) {
-  output <- match.arg(output)
-  # Validate query
-  if (!is.character(query) || length(query) != 1L) {
-    stop("query must be a single character string")
-  }
-  query <- trimws(query)
-  if (nchar(query) == 0L) {
-    stop("query must be a non-empty string")
-  }
-
-  # Query using httr2 from DuckDuckGo Instant Answer API:
-  # https://api.duckduckgo.com/?q={query}&format=json
-  req <- httr2::request("https://duckduckgo.com/") |>
-    httr2::req_url_query(
-      q = query,
-      ia = "web"
-    ) |>
-    # Add user agent
-    httr2::req_user_agent("Kaimana (kaimana.rtemis.org)")
-  # Perform request
-  res <- httr2::req_perform(req)
-  # Check for HTTP errors
-  httr2::resp_check_status(res)
-  # Parse response to JSON (resp_body_json fails here)
-  res_raw <- httr2::resp_body_string(res)
-  # Scrape the results from the HTML using rvest
-  scraped <- rvest::read_html(res_raw)
-  rvest::html_text2(res_raw)
-  # Convert to list
-  res_list <- jsonlite::fromJSON(res_raw, simplifyVector = TRUE)
-  # Build data.table
-  if (return_all) {
-    return(res_list)
-  }
-  dat <- data.table(
-    Abstract_text = res_list[["AbstractText"]],
-    Abstract_URL = res_list[["AbstractURL"]]
-  )
-  if (nchar(res_list[["Answer"]]) > 0L) {
-    dat[, Answer := res_list[["Answer"]]]
-    dat[, Answer_type := res_list[["AnswerType"]]]
-  }
-  # Convert to JSON
-  if (output == "json") {
-    return(jsonlite::toJSON(dat, pretty = FALSE, auto_unbox = TRUE))
-  }
-  dat
-} # /query_duckduckgo_ia
+} # /kaimana::query_duckduckgo_ia
 
 
 # %% tool_duckduckgo_ia ----
